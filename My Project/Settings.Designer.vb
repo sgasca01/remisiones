8a20f2b7ffabc16a9a987b9aaeb08c07ddfa874a
sgasca01 <sgasca@lucta.com>
2021-06-03 10:35:50 -0500

    Final version
    Final version

--- My Project/Settings.Designer.vb
+++ My Project/Settings.Designer.vb
@@ -64,9 +64,9 @@ Namespace My
     Friend Module MySettingsProperty
         
         <Global.System.ComponentModel.Design.HelpKeywordAttribute("My.Settings")>  _
-        Friend ReadOnly Property Settings() As Global.cc.My.MySettings
+        Friend ReadOnly Property Settings() As Global.rm.My.MySettings
             Get
-                Return Global.cc.My.MySettings.Default
+                Return Global.rm.My.MySettings.Default
             End Get
         End Property
     End Module
